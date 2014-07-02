e6f0891f2fb050024cff050d351957790ff02d1b
Dan Mulloy <dmulloy2@live.com>
2014-07-01 22:14:02 -0400

    Cleanup TimeUtil, add javadocs

--- src/main/java/net/dmulloy2/util/TimeUtil.java
+++ src/main/java/net/dmulloy2/util/TimeUtil.java
@@ -14,25 +14,42 @@ import net.dmulloy2.exception.BadTimeException;
 
 /**
  * Util for dealing with time.
- * 
+ *
  * @author dmulloy2
  */
 
-public final class TimeUtil
+public class TimeUtil
 {
 	private TimeUtil() { }
 
-	public static String formatTimeDifference(long time1, long time2)
+	/**
+	 * Returns the formatted time difference between two times
+	 *
+	 * @param time1 First time in milliseconds
+	 * @param time2 Second time in milliseconds
+	 * @return Formatted time difference
+	 */
+	public static final String formatTimeDifference(long time1, long time2)
 	{
-		return formatTime(getTimeDifference(time1, time2));
+		return formatTime(Math.abs(time2 - time1));
 	}
 
-	public static long getTimeDifference(long time1, long time2)
+	/**
+	 * @deprecated This is just simple subtraction
+	 */
+	@Deprecated
+	public static final long getTimeDifference(long time1, long time2)
 	{
-		return (time2 - time1);
+		return time2 - time1;
 	}
 
-	public static String formatTime(long time)
+	/**
+	 * Formats a given time
+	 *
+	 * @param time Time in milliseconds
+	 * @return Formatted time
+	 */
+	public static final String formatTime(long time)
 	{
 		StringBuilder ret = new StringBuilder();
 		int days = (int) Math.floor(time / (1000 * 3600 * 24));
@@ -51,32 +68,66 @@ public final class TimeUtil
 		return ret.toString();
 	}
 
-	public static String getLongDateCurr()
+	/**
+	 * Gets the current long date in a given time zone
+	 *
+	 * @param timeZone Time zone, defaults to GMT
+	 * @return The current long date
+	 */
+	public static final String getLongDateCurr(String timeZone)
 	{
 		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy HH:mm");
 		Date date = new Date();
-		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		dateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
 		return dateFormat.format(date);
 	}
 
-	public static String getSimpleDate(long time)
+	/**
+	 * Gets the current long date in GMT
+	 *
+	 * @see {@link TimeUtil#getLongDateCurr(String)}
+	 */
+	public static final String getLongDateCurr()
+	{
+		return getLongDateCurr("GMT");
+	}
+
+	public static final String getSimpleDate(long time, String timeZone)
 	{
 		DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
 		Date date = new Date(time);
-		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		dateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
 		return dateFormat.format(date);
 	}
 
-	public static long parseTime(String t) throws BadTimeException
+	/**
+	 * Gets the simple date of a given time in GMT
+	 *
+	 * @param time Time in milliseconds
+	 * @see {@link TimeUtil#getSimpleDate(long, String)}
+	 */
+	public static final String getSimpleDate(long time)
+	{
+		return getSimpleDate(time, "GMT");
+	}
+
+	/**
+	 * Parses a given time
+	 *
+	 * @param time Time
+	 * @return The parsed time
+	 * @throws BadTimeException If parsing fails
+	 */
+	public static final long parseTime(String time) throws BadTimeException
 	{
 		try
 		{
-			if (! t.matches("[0-9]+d[a-z]*"))
-				return Math.round(Double.parseDouble(t) * 60) * 1000;
+			if (! time.matches("[0-9]+d[a-z]*"))
+				return Math.round(Double.parseDouble(time) * 60) * 1000;
 			else
 			{
 				Pattern dayPattern = Pattern.compile("([0-9]+)d[a-z]*", Pattern.CASE_INSENSITIVE);
-				Matcher m = dayPattern.matcher(t);
+				Matcher m = dayPattern.matcher(time);
 
 				if (m.matches())
 				{
@@ -84,27 +135,27 @@ public final class TimeUtil
 				}
 			}
 		}
-		catch (NumberFormatException e)
+		catch (NumberFormatException ex)
 		{
 			Pattern hourPattern = Pattern.compile("([0-9]+)h[a-z]*", Pattern.CASE_INSENSITIVE);
 			Pattern minPattern = Pattern.compile("([0-9]+)m[a-z]*", Pattern.CASE_INSENSITIVE);
 			Pattern secPattern = Pattern.compile("([0-9]+)s[a-z]*", Pattern.CASE_INSENSITIVE);
 
-			Matcher m = hourPattern.matcher(t);
+			Matcher m = hourPattern.matcher(time);
 
 			if (m.matches())
 			{
 				return Integer.parseInt(m.group(1)) * 60 * 60 * 1000;
 			}
 
-			m = minPattern.matcher(t);
+			m = minPattern.matcher(time);
 
 			if (m.matches())
 			{
 				return Integer.parseInt(m.group(1)) * 60 * 1000;
 			}
 
-			m = secPattern.matcher(t);
+			m = secPattern.matcher(time);
 
 			if (m.matches())
 			{
@@ -118,12 +169,12 @@ public final class TimeUtil
 
 	// ---- Conversion Methods
 
-	public static long toTicks(Number seconds)
+	public static final long toTicks(Number seconds)
 	{
 		return seconds.intValue() * 20;
 	}
 
-	public static int toSeconds(Number ticks)
+	public static final int toSeconds(Number ticks)
 	{
 		return ticks.intValue() / 20;
 	}
