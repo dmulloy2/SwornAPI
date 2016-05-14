b2f3bf67f823c56bc720777a913027bcff1d4761
Dan Mulloy <dmulloy2@live.com>
2016-05-14 11:34:31 -0400

    Update to 1.9.4

--- src/test/java/net/dmulloy2/BukkitTesting.java
+++ src/test/java/net/dmulloy2/BukkitTesting.java
@@ -8,15 +8,15 @@ import static org.mockito.Mockito.when;
 
 import java.util.logging.Logger;
 
-import net.minecraft.server.v1_9_R1.DispenserRegistry;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
-import org.bukkit.craftbukkit.v1_9_R1.CraftServer;
-import org.bukkit.craftbukkit.v1_9_R1.inventory.CraftItemFactory;
-import org.bukkit.craftbukkit.v1_9_R1.util.Versioning;
+import org.bukkit.craftbukkit.v1_9_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_9_R2.inventory.CraftItemFactory;
+import org.bukkit.craftbukkit.v1_9_R2.util.Versioning;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
+import net.minecraft.server.v1_9_R2.DispenserRegistry;
 
 /**
  * @author dmulloy2
